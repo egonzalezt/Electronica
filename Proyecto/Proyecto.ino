10c20e4a03b86d5ed02c97a88142f0d9ea8ab6e7
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2019-11-14 19:56:41 -0500

    Update Proyecto.ino

--- Proyecto/Proyecto.ino
+++ Proyecto/Proyecto.ino
@@ -8,24 +8,20 @@
 //libraries
 #include <SPI.h>
 #include <MFRC522.h>
-
+#include <Blynk.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 //Pin Connection
 #define relayInput D1 // the input to the relay pin
 #define SS_PIN D4  //D2
 #define RST_PIN D2 //D1
 
-#include <Blynk.h>
-
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 char auth[] = "pzWcYBtaQLGaWdnwtSRcg4sJFsqjzwDv";
 char ssid[] = "WISPEREXT";
 char pass[] = "wispergt2020";
-
-
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 int statuss = 2;
 int out = 0;
@@ -40,27 +36,26 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
+  Blynk.virtualWrite(V4, "Welcome");
+  Blynk.virtualWrite(V5, " Opedor System");
 }
 
 void loop() 
 {
   Blynk.run();
   // Look for new cards
-  if (mfrc522.PICC_IsNewCardPresent()) 
-  {
-    Serial.print("Hola");
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  if (!mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
   }
-  if(statuss==1)
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial()) 
   {
-    Serial.println(".");
+    return;
   }
-  else
+  if(statuss!=1)
   {
+    Blynk.virtualWrite(V4, " Opedor System");
     check(cardRd());
   }
 }
@@ -92,12 +87,16 @@ void check(String value)
       Serial.print(value.equals(tam[j]));
       if (value.equals(tam[j])==1&&j<=4) //condition to check if the card can open the door
       {
+        Blynk.virtualWrite(V4, "Welcome Door");
+        Blynk.virtualWrite(V5, "Open");
         Serial.println("Access Granted ");
         Serial.println("Welcome ");
         Serial.println("Door unlocked");
         digitalWrite(relayInput, HIGH); //turn ralay on to activate the solenoid
         delay(5000);
         digitalWrite(relayInput, LOW); // turn relay off
+        Blynk.virtualWrite(V4, "Door");
+        Blynk.virtualWrite(V5, "Locked");
         Serial.println("Door locked");
         Serial.println();
         statuss = 1;
@@ -105,13 +104,16 @@ void check(String value)
       }
       else
       {
-        Serial.println("Access Denied");  
+        Serial.println("Access Denied");
+        Blynk.virtualWrite(V4, "Access Denied");  
       }
     }   
   }
+  Blynk.virtualWrite(V4, " Opedor System");
+  Blynk.virtualWrite(V5, " ");
 }
 
-void add1(String value)
+void Add(String value)
 {
   if(value=="")
   {
@@ -125,9 +127,12 @@ void add1(String value)
       {
         if(tam[i]=="")
         {
+          Blynk.virtualWrite(V4, "Adding card");
           Serial.println("Adding card");
           tam[i]= value;
+          Blynk.virtualWrite(V4, "Card added");
           Serial.println("Success!! card added");
+          Blynk.virtualWrite(V5, "Remove the card");
           Serial.println("please remove the card from the reader");
           break;
         }  
@@ -139,6 +144,8 @@ void add1(String value)
     }
   }
   delay(2000);
+  Blynk.virtualWrite(V4, " Opedor System");
+  Blynk.virtualWrite(V5, " ");
 }
 
 void Delete(String value)
@@ -153,9 +160,12 @@ void Delete(String value)
     {
       if(value.equals(tam[i])==1&&i<4)
       {
+        Blynk.virtualWrite(V4, "Deleting card");
         Serial.println("Deleting card");
         tam[i]= "";
+        Blynk.virtualWrite(V4, "Card deleted");
         Serial.println("Success!! card deleted");
+        Blynk.virtualWrite(V5, "Remove the card");
         Serial.println("please remove the card from the reader"); 
         break;
       }
@@ -166,6 +176,8 @@ void Delete(String value)
     }
   }
   delay(2000);
+  Blynk.virtualWrite(V4, " Opedor System");
+  Blynk.virtualWrite(V5, " ");
 }
 
 BLYNK_WRITE(V1) 
@@ -174,15 +186,21 @@ BLYNK_WRITE(V1)
   int a = param.asInt();
   if (a == 1) 
   {
+    Blynk.virtualWrite(V4, "Put the card");
+    Blynk.virtualWrite(V5, "on the reader");
     Serial.println("Please put the card on the reader");
     delay(2000);
     if(!mfrc522.PICC_IsNewCardPresent())
     {
+      Blynk.virtualWrite(V4, "Card");
+      Blynk.virtualWrite(V5, "not detected");
       Serial.println("Card not detected"); 
     }
     else
     {
-      Delete(cardRd());      
+      Delete(cardRd()); 
+      Blynk.virtualWrite(V4, " Opedor System");
+      Blynk.virtualWrite(V5, " ");     
     }
   }
   statuss=0; 
@@ -195,14 +213,20 @@ BLYNK_WRITE(V2)
   if (a == 1) 
   {
     Serial.println("Please put the card on the reader");
+    Blynk.virtualWrite(V4, "Put the card");
+    Blynk.virtualWrite(V5, "on the reader");
     delay(2000);
     if(!mfrc522.PICC_IsNewCardPresent())
     {
+      Blynk.virtualWrite(V4, "Card");
+      Blynk.virtualWrite(V5, "not detected");
       Serial.println("Card not detected"); 
     }
     else
     {
-      add1(cardRd());
+      Add(cardRd());
+      Blynk.virtualWrite(V4, " Opedor System");
+      Blynk.virtualWrite(V5, " ");
     }
   }
   statuss=0; 
@@ -217,13 +241,23 @@ BLYNK_WRITE(V3)
     Serial.println("Access Granted ");
     Serial.println("Welcome ");
     Serial.println("Door unlocked");
+    Blynk.virtualWrite(V4, "Access Granted");
+    delay(500);
+    Blynk.virtualWrite(V5, "Door unlocked");
+    Blynk.virtualWrite(V4, "Welcome       ");
     digitalWrite(relayInput, HIGH); //turn ralay on to activate the solenoid
+    delay(2000);
+    Blynk.virtualWrite(V4, " Opedor System");
+    Blynk.virtualWrite(V5, " ");
   } 
   if(a==0)
   {
+    Blynk.virtualWrite(V4, "Door locked");
     digitalWrite(relayInput, LOW); // turn relay off
     Serial.println("Door locked");
     Serial.println();
+    delay(2000);
+    Blynk.virtualWrite(V4, " Opedor System");
   }
   statuss=0;
 }
